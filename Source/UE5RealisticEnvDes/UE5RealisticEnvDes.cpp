6857ac339d539d6cad51f9a3a423fae5f8c50573
Jacob Mott <jacobmott@gmail.com>
2022-07-21 07:39:17 +0100

    Added ocean, and lighting to the scene

---
+++ Source/UE5RealisticEnvDes/UE5RealisticEnvDes.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE5RealisticEnvDes.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5RealisticEnvDes, "UE5RealisticEnvDes" );
